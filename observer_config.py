8a571bcba76e978c99282bf9eedd81e931fd727c
PsoyNe <164649497+PsoyNe@users.noreply.github.com>
2026-09-09 21:03:54 +0300

    Update observer_config.py

--- observer_config.py
+++ observer_config.py
@@ -15,16 +15,17 @@ LOG_FILE = "/var/log/vless_observer.log"
 # -------------------- НАСТРОЙКИ МОНИТОРИНГА --------------------
 CHECK_INTERVAL_PRIMARY = 5        # Проверка основной ссылки (сек)
 CHECK_INTERVAL_BACKUP = 30        # Проверка резервных ссылок (сек)
-FAILURES_TO_SWITCH = 2            # Отказов подряд для переключения
+FAILURES_TO_SWITCH = 5            # ← ИЗМЕНЕНО: 5 отказов подряд (было 2)
 BACKUP_POOL_SIZE = 4              # Размер резервного пула
 POOL_UPDATE_INTERVAL = 21600      # Обновление пула из файла (сек) = 6 часов
 
 # -------------------- НАСТРОЙКИ ТЕСТИРОВАНИЯ --------------------
-TEST_HOST = 'www.google.com'
+TEST_HOST = 'check.torproject.org'
 TEST_PORT = 443
-TEST_PATH = '/generate_204'
-TEST_TIMEOUT = 5
-TEST_BUFFER_SIZE = 1024
+TEST_PATH = '/'
+TEST_TIMEOUT = 10
+TEST_BUFFER_SIZE = 65536
+TEST_EXPECTED_STRING = b'You are not using Tor'
 
 # -------------------- НАСТРОЙКИ 3X-UI --------------------
 OUTBOUND_NAME = "vless_obs"
